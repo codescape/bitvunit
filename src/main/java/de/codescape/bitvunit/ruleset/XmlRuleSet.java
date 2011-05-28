9cddf973a32bce52202bcb93de9c30d71a28eb46
Stefan Glase <stefan.glase@googlemail.com>
2011-05-28 17:53:16 +0200

    refactoring of XmlRuleSet for better readability

--- src/main/java/de/codescape/bitvunit/ruleset/XmlRuleSet.java
+++ src/main/java/de/codescape/bitvunit/ruleset/XmlRuleSet.java
@@ -9,34 +9,37 @@ import javax.xml.parsers.DocumentBuilderFactory;
 
 public class XmlRuleSet extends BasicRuleSet implements RuleSet {
 
-    private DocumentBuilderFactory documentFactory = DocumentBuilderFactory.newInstance();
+    private static DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
 
-    public XmlRuleSet(String location) throws XmlRuleSetException {
+    public XmlRuleSet(String location) {
         super();
-        addAllRulesFromDocument(createDocument(location));
+        addRulesFromDocument(buildDocument(location));
     }
 
-    private void addAllRulesFromDocument(Document document) {
-        NodeList ruleNodes = document.getElementsByTagName("rule");
-        for (int i = 0; i < ruleNodes.getLength(); i++) {
-            addRuleFromNode(ruleNodes.item(i));
-        }
-    }
-
-    private void addRuleFromNode(Node node) {
-        String ruleClassName = node.getAttributes().getNamedItem("class").getTextContent();
+    private Document buildDocument(String location) {
         try {
-            addRule((Rule) Class.forName(ruleClassName).newInstance());
-        } catch (Exception e) {
-            throw new XmlRuleSetException("Could not create Rule.", e);
-        }
-    }
-
-    private Document createDocument(String location) {
-        try {
-            return documentFactory.newDocumentBuilder().parse(this.getClass().getResource(location).getFile());
+            return documentBuilderFactory.newDocumentBuilder().parse(this.getClass().getResource(location).getFile());
         } catch (Exception e) {
             throw new XmlRuleSetException("Could not parse RuleSet from given location.", e);
+        }
+    }
+
+    private void addRulesFromDocument(Document document) {
+        NodeList ruleNodes = document.getElementsByTagName("rule");
+        for (int i = 0; i < ruleNodes.getLength(); i++) {
+            addRule(extractClassAttributeFromNode(ruleNodes.item(i)));
+        }
+    }
+
+    private String extractClassAttributeFromNode(Node node) {
+        return node.getAttributes().getNamedItem("class").getTextContent();
+    }
+
+    private void addRule(String className) {
+        try {
+            addRule((Rule) Class.forName(className).newInstance());
+        } catch (Exception e) {
+            throw new XmlRuleSetException("Could not instantiate rule " + className + ".", e);
         }
     }
 
