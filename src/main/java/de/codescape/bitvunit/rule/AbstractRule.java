be7a54c92a172f0b8248cc612ab4744d25d2608d
Stefan Glase <stefan.glase@googlemail.com>
2011-05-30 21:13:06 +0200

    added javadoc to the AbstractRule class

--- src/main/java/de/codescape/bitvunit/rule/AbstractRule.java
+++ src/main/java/de/codescape/bitvunit/rule/AbstractRule.java
@@ -6,11 +6,26 @@ import de.codescape.bitvunit.model.Page;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Base class to all implementations of the Rule interface. It provides convenience methods to create violations within
+ * the implementation of a rule and allows to apply a rule to a given HtmlPage object.
+ */
 public abstract class AbstractRule implements Rule {
 
+    /**
+     * Returns the name of that rule.
+     *
+     * @return name of that rule
+     */
     @Override
     public abstract String getName();
 
+    /**
+     * Applies that rule to the given HtmlPage and returns a list of violations.
+     *
+     * @param htmlPage HtmlPage under test
+     * @return list of violations
+     */
     @Override
     public final List<Violation> applyTo(HtmlPage htmlPage) {
         List<Violation> violations = new ArrayList<Violation>();
@@ -18,16 +33,32 @@ public abstract class AbstractRule implements Rule {
         return violations;
     }
 
+    /**
+     * Internal method to apply that rule to a given Page which provides some convenience methods to allocate tags.
+     *
+     * @param page       page under test
+     * @param violations list of violations
+     */
     protected abstract void applyTo(Page page, List<Violation> violations);
 
-    protected Violation createViolation(Rule rule, int lineNumber, String message) {
-        return new Violation(rule, lineNumber, message);
-    }
-
+    /**
+     * Creates a new violation for that rule with the line number of the violating element in the given page and a
+     * message that describes the violation in detail.
+     *
+     * @param lineNumber line number where the violation occurs
+     * @param message    description of the violation
+     * @return new violation to be added to the list of violations
+     */
     protected Violation createViolation(int lineNumber, String message) {
-        return createViolation(this, lineNumber, message);
+        return new Violation(this, lineNumber, message);
     }
 
+    /**
+     * Returns a String representation of the name of that rule. Per default this results in a call of the getName()
+     * method.
+     *
+     * @return name of that rule
+     */
     @Override
     public String toString() {
         return getName();
