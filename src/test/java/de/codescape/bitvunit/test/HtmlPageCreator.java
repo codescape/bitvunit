71eaccce5efc4cf11932fe1b05df151cf1329a71
Stefan Glase <stefan.glase@googlemail.com>
2011-06-03 17:40:27 +0200

    using the HtmlUnit way to create a HtmlPage from a String

--- src/test/java/de/codescape/bitvunit/test/HtmlPageCreator.java
+++ src/test/java/de/codescape/bitvunit/test/HtmlPageCreator.java
@@ -1,8 +1,8 @@
 package de.codescape.bitvunit.test;
 
-import com.gargoylesoftware.htmlunit.MockWebConnection;
-import com.gargoylesoftware.htmlunit.ThreadedRefreshHandler;
+import com.gargoylesoftware.htmlunit.StringWebResponse;
 import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HTMLParser;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
 import java.io.IOException;
@@ -12,26 +12,20 @@ import java.net.URL;
 public abstract class HtmlPageCreator {
 
     public static HtmlPage createHtmlPage(String content) {
-        URL fakeURL = createFakeURL();
-        try {
-            return createWebClient(content, fakeURL).getPage(fakeURL);
-        } catch (IOException e) {
-            throw new RuntimeException("Error creating HtmlPage with given content.", e);
-        }
+        return createHtmlPage(new StringWebResponse(content, createFakeURL()));
     }
 
-    private static WebClient createWebClient(String content, URL fakeURL) {
-        WebClient webClient = new WebClient();
-        MockWebConnection mockWebConnection = new MockWebConnection();
-        mockWebConnection.setResponse(fakeURL, content);
-        webClient.setWebConnection(mockWebConnection);
-        webClient.setRefreshHandler(new ThreadedRefreshHandler());
-        return webClient;
+    private static HtmlPage createHtmlPage(StringWebResponse response) {
+        try {
+            return HTMLParser.parseHtml(response, new WebClient().getCurrentWindow());
+        } catch (IOException e) {
+            throw new RuntimeException("Error creating HtmlPage.", e);
+        }
     }
 
     private static URL createFakeURL() {
         try {
-            return new URL("http://localhost:8080/page.htm");
+            return new URL("http://bitvunit.codescape.de");
         } catch (MalformedURLException e) {
             throw new RuntimeException("Error creating fake URL.", e);
         }
