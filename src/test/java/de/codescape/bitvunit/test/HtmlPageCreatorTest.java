b966545fe9d90c2b04b009400088d95875adb81e
Stefan Glase <stefan.glase@googlemail.com>
2011-06-03 17:46:30 +0200

    added test for HtmlPageCreator

---
+++ src/test/java/de/codescape/bitvunit/test/HtmlPageCreatorTest.java
@@ -0,0 +1,19 @@
+package de.codescape.bitvunit.test;
+
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import org.junit.Test;
+
+import static de.codescape.bitvunit.test.HtmlPageCreator.createHtmlPage;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+public class HtmlPageCreatorTest {
+
+    @Test
+    public void creatingAHtmlPage() throws Exception {
+        String content = "<html><body><p id=\"text\">Hello World</p></body></html>";
+        HtmlPage htmlPage = createHtmlPage(content);
+        assertEquals("Hello World", htmlPage.getElementById("text").getTextContent());
+    }
+
+}
