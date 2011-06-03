5c3e5a54703a8b6e67167df6d20ec97018995529
Stefan Glase <stefan.glase@googlemail.com>
2011-06-03 16:08:28 +0200

    uppercase/lowercase rename (1/2)

---
+++ src/main/java/de/codescape/bitvunit/ruleset/XmlRuleSetException_RENAME.java
@@ -0,0 +1,9 @@
+package de.codescape.bitvunit.ruleset;
+
+public class XmlRuleSetException extends RuntimeException {
+
+    public XmlRuleSetException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
