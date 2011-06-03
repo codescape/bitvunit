05b7c6d2e74870493b014837974b0a0eff113d96
Stefan Glase <stefan.glase@googlemail.com>
2011-06-03 21:56:08 +0200

    added test for single rule checks

--- bitvunit-test-integration/src/test/java/de/codescape/bitvunit/test/integration/JUnitAndHamcrestIntegrationTest.java
+++ bitvunit-test-integration/src/test/java/de/codescape/bitvunit/test/integration/JUnitAndHamcrestIntegrationTest.java
@@ -1,10 +1,11 @@
 package de.codescape.bitvunit.test.integration;
 
-import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import de.codescape.bitvunit.rule.text.LanguageForHtmlTagRule;
 import de.codescape.bitvunit.ruleset.XmlRuleSet;
 import org.junit.Test;
 
+import static de.codescape.bitvunit.hamcrest.RuleMatcher.complaintTo;
 import static de.codescape.bitvunit.hamcrest.RuleSetMatcher.complaintTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -12,9 +13,15 @@ import static org.junit.Assert.assertThat;
 public class JUnitAndHamcrestIntegrationTest extends AbstractIntegrationTest {
 
     @Test
-    public void checkFirstPageOfDocumentation() throws Exception {
+    public void checkFirstPageOfDocumentationAgainstRuleSet() throws Exception {
         HtmlPage page = webClient.getPage(BITVUNIT_DOCUMENTATION_URL);
         assertThat(page, is(complaintTo(new XmlRuleSet("/rulesets/all-rules.xml"))));
+    }
+
+    @Test
+    public void checkFirstPageOfDocumentationAgainstRule() throws Exception {
+        HtmlPage page = webClient.getPage(BITVUNIT_DOCUMENTATION_URL);
+        assertThat(page, is(complaintTo(new LanguageForHtmlTagRule())));
     }
 
 }
--- bitvunit-test-integration/src/test/java/de/codescape/bitvunit/test/integration/JUnitIntegrationTest.java
+++ bitvunit-test-integration/src/test/java/de/codescape/bitvunit/test/integration/JUnitIntegrationTest.java
@@ -1,7 +1,7 @@
 package de.codescape.bitvunit.test.integration;
 
-import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import de.codescape.bitvunit.rule.text.LanguageForHtmlTagRule;
 import de.codescape.bitvunit.ruleset.XmlRuleSet;
 import org.junit.Test;
 
@@ -10,9 +10,15 @@ import static org.junit.Assert.assertFalse;
 public class JUnitIntegrationTest extends AbstractIntegrationTest {
 
     @Test
-    public void checkFirstPageOfDocumentation() throws Exception {
+    public void checkFirstPageOfDocumentationAgainstRuleSet() throws Exception {
         HtmlPage page = webClient.getPage(BITVUNIT_DOCUMENTATION_URL);
         assertFalse(new XmlRuleSet("/rulesets/all-rules.xml").applyTo(page).hasViolations());
+    }
+
+    @Test
+    public void checkFirstPageOfDocumentationAgainstRule() throws Exception {
+        HtmlPage page = webClient.getPage(BITVUNIT_DOCUMENTATION_URL);
+        assertFalse(new LanguageForHtmlTagRule().applyTo(page).hasViolations());
     }
 
 }
