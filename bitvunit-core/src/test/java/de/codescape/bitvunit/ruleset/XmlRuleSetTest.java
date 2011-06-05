136de31c81c0dcdc5d16a8cd9e750cfe30296625
Stefan Glase <stefan.glase@googlemail.com>
2011-06-05 14:46:44 +0200

    removed depdency to Google Reflections and tests using it

--- bitvunit-core/src/test/java/de/codescape/bitvunit/ruleset/XmlRuleSetTest.java
+++ bitvunit-core/src/test/java/de/codescape/bitvunit/ruleset/XmlRuleSetTest.java
@@ -1,36 +1,22 @@
 package de.codescape.bitvunit.ruleset;
 
-import de.codescape.bitvunit.rule.Rule;
 import org.junit.Test;
-import org.reflections.Reflections;
-
-import java.lang.reflect.Modifier;
 
 import static de.codescape.bitvunit.test.Assertions.assertRules;
 
 public class XmlRuleSetTest {
 
-    private static final String PACKAGE_FOR_RULES = "de.codescape.bitvunit.rule";
+    public static final int NUMBER_OF_RULES = 18;
 
     @Test
     public void creationOfRuleSetAllRulesFindsAllRules() throws Exception {
         RuleSet ruleSet = new XmlRuleSet("/rulesets/all-rules.xml");
-        assertRules(ruleSet, numberOfRulesInClasspath());
+        assertRules(ruleSet, NUMBER_OF_RULES);
     }
 
     @Test(expected = XmlRuleSetException.class)
     public void missingRuleSetLeadsToXmlRuleSetException() throws Exception {
         new XmlRuleSet("/rulesets/missing-ruleset.xml");
-    }
-
-    private int numberOfRulesInClasspath() {
-        int count = 0;
-        for (Class<? extends Rule> ruleClass : new Reflections(PACKAGE_FOR_RULES).getSubTypesOf(Rule.class)) {
-            if (!Modifier.isAbstract(ruleClass.getModifiers())) {
-                count++;
-            }
-        }
-        return count;
     }
 
 }
