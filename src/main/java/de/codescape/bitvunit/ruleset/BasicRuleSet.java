6c0f7d37bcb3ab4a145f91042e83357cfb2798a6
Stefan Glase <stefan.glase@googlemail.com>
2011-06-17 14:39:33 +0200

    added javadoc for BasicRuleSet

--- src/main/java/de/codescape/bitvunit/ruleset/BasicRuleSet.java
+++ src/main/java/de/codescape/bitvunit/ruleset/BasicRuleSet.java
@@ -9,10 +9,19 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * Basic implementation of a {@link RuleSet} that provides some convenience methods. This class should normally be
+ * extended by other {@link RuleSet} implementations.
+ */
 public class BasicRuleSet implements RuleSet {
 
     private List<Rule> rules = new ArrayList<Rule>();
 
+    /**
+     * Adds the given {@link Rule} to that rule set.
+     *
+     * @param rule {@link Rule} to be added to the rule set
+     */
     public void addRule(Rule rule) {
         rules.add(rule);
     }
