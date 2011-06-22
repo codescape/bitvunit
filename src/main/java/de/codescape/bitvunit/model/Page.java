75e1e82317fd6f434c524b99be45ff27c1092970
Stefan Glase <stefan.glase@googlemail.com>
2011-06-22 20:39:02 +0200

    slightly easier version of the page selectors

--- src/main/java/de/codescape/bitvunit/model/Page.java
+++ src/main/java/de/codescape/bitvunit/model/Page.java
@@ -2,7 +2,6 @@ package de.codescape.bitvunit.model;
 
 import com.gargoylesoftware.htmlunit.html.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Page {
@@ -14,68 +13,64 @@ public class Page {
     }
 
     @SuppressWarnings("unchecked")
-    private <T extends HtmlElement> List<T> findHtmlElementsByTagName(String tagName) {
-        List<T> result = new ArrayList<T>();
-        for (HtmlElement element : htmlPage.getElementsByTagName(tagName)) {
-            result.add((T) element);
-        }
-        return result;
+    private <T extends HtmlElement> List<T> allByTagName(String tagName) {
+        return (List<T>)htmlPage.getElementsByTagName(tagName);
     }
 
     public List<HtmlLabel> findAllLabelTags() {
-        return findHtmlElementsByTagName(HtmlLabel.TAG_NAME);
+        return allByTagName(HtmlLabel.TAG_NAME);
     }
 
     public List<HtmlInput> findAllInputTags() {
-        return findHtmlElementsByTagName(HtmlInput.TAG_NAME);
+        return allByTagName(HtmlInput.TAG_NAME);
     }
 
     public List<HtmlSelect> findAllSelectTags() {
-        return findHtmlElementsByTagName(HtmlSelect.TAG_NAME);
+        return allByTagName(HtmlSelect.TAG_NAME);
     }
 
     public List<HtmlTextArea> findAllTextareaTags() {
-        return findHtmlElementsByTagName(HtmlTextArea.TAG_NAME);
+        return allByTagName(HtmlTextArea.TAG_NAME);
     }
 
     public List<HtmlImage> findAllImageTags() {
-        return findHtmlElementsByTagName(HtmlImage.TAG_NAME);
+        return allByTagName(HtmlImage.TAG_NAME);
     }
 
     public List<HtmlTable> findAllTableTags() {
-        return findHtmlElementsByTagName(HtmlTable.TAG_NAME);
+        return allByTagName(HtmlTable.TAG_NAME);
     }
 
     public List<HtmlBlink> findAllBlinkTags() {
-        return findHtmlElementsByTagName(HtmlBlink.TAG_NAME);
+        return allByTagName(HtmlBlink.TAG_NAME);
     }
 
     public List<HtmlMarquee> findAllMarqueeTags() {
-        return findHtmlElementsByTagName(HtmlMarquee.TAG_NAME);
+        return allByTagName(HtmlMarquee.TAG_NAME);
     }
 
     public List<HtmlAbbreviated> findAllAbbreviationTags() {
-        return findHtmlElementsByTagName(HtmlAbbreviated.TAG_NAME);
+        return allByTagName(HtmlAbbreviated.TAG_NAME);
     }
 
     public List<HtmlAcronym> findAllAcronymTags() {
-        return findHtmlElementsByTagName(HtmlAcronym.TAG_NAME);
+        return allByTagName(HtmlAcronym.TAG_NAME);
     }
 
     public List<HtmlBold> findAllBoldTags() {
-        return findHtmlElementsByTagName(HtmlBold.TAG_NAME);
+        return allByTagName(HtmlBold.TAG_NAME);
     }
 
     public List<HtmlItalic> findAllItalicTags() {
-        return findHtmlElementsByTagName(HtmlItalic.TAG_NAME);
+        return allByTagName(HtmlItalic.TAG_NAME);
     }
 
     public List<HtmlHtml> findAllHtmlTags() {
-        return findHtmlElementsByTagName(HtmlHtml.TAG_NAME);
+        return allByTagName(HtmlHtml.TAG_NAME);
     }
 
     public List<HtmlTableCell> findAllTableHeaders() {
-        return findHtmlElementsByTagName("th");
+        return allByTagName("th");
     }
 
     @SuppressWarnings("unchecked")
@@ -84,19 +79,19 @@ public class Page {
     }
 
     public List<HtmlMeta> findAllMetaTags() {
-        return findHtmlElementsByTagName(HtmlMeta.TAG_NAME);
+        return allByTagName(HtmlMeta.TAG_NAME);
     }
 
     public List<HtmlUnorderedList> findAllUnorderedLists() {
-        return findHtmlElementsByTagName(HtmlUnorderedList.TAG_NAME);
+        return allByTagName(HtmlUnorderedList.TAG_NAME);
     }
 
     public List<HtmlOrderedList> findAllOrderedLists() {
-        return findHtmlElementsByTagName(HtmlOrderedList.TAG_NAME);
+        return allByTagName(HtmlOrderedList.TAG_NAME);
     }
 
     public List<HtmlInlineFrame> findAllInlineFrameTags() {
-        return findHtmlElementsByTagName(HtmlInlineFrame.TAG_NAME);
+        return allByTagName(HtmlInlineFrame.TAG_NAME);
     }
 
     public HtmlElement findHtmlElementById(String elementId) {
@@ -104,15 +99,15 @@ public class Page {
     }
 
     public List<HtmlFrame> findAllFrameTags() {
-        return findHtmlElementsByTagName(HtmlFrame.TAG_NAME);
+        return allByTagName(HtmlFrame.TAG_NAME);
     }
 
     public List<HtmlArea> findAllAreaTags() {
-        return findHtmlElementsByTagName(HtmlArea.TAG_NAME);
+        return allByTagName(HtmlArea.TAG_NAME);
     }
 
     public List<HtmlApplet> findAllAppletTags() {
-        return findHtmlElementsByTagName(HtmlApplet.TAG_NAME);
+        return allByTagName(HtmlApplet.TAG_NAME);
     }
 
 }
