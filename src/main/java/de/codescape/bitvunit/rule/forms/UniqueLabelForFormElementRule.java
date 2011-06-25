95685b519bba46bd0e0a1c09702715993a4affe1
Stefan Glase <stefan.glase@googlemail.com>
2011-06-25 19:18:56 +0200

    added new rule UniqueLabelForFormElementRule

---
+++ src/main/java/de/codescape/bitvunit/rule/forms/UniqueLabelForFormElementRule.java
@@ -0,0 +1,47 @@
+package de.codescape.bitvunit.rule.forms;
+
+import com.gargoylesoftware.htmlunit.html.HtmlLabel;
+import de.codescape.bitvunit.model.Page;
+import de.codescape.bitvunit.rule.AbstractRule;
+import de.codescape.bitvunit.rule.Violations;
+
+import java.util.List;
+
+/**
+ * UniqueLabelForFormElementRule ensures that there is never more than one <code>&lt;label /&gt;</code> with the same
+ * value in it's <code>for</code> attribute. There should be never be more than one label pointing to a single form
+ * element.
+ *
+ * @since 0.3
+ */
+public class UniqueLabelForFormElementRule extends AbstractRule {
+
+    private static final String RULE_NAME = "UniqueLabelForFormElement";
+    private static final String RULE_MESSAGE = "There should be never be more than one label pointing to a single form element.";
+
+    @Override
+    public String getName() {
+        return RULE_NAME;
+    }
+
+    @Override
+    protected void applyTo(Page page, Violations violations) {
+        List<HtmlLabel> labels = page.findAllLabelTags();
+        for (HtmlLabel label : labels) {
+            if (!exactlyOneLabelWithGivenForAttribute(labels, label.getForAttribute())) {
+                violations.add(createViolation(label, RULE_MESSAGE));
+            }
+        }
+    }
+
+    private boolean exactlyOneLabelWithGivenForAttribute(List<HtmlLabel> labels, String forAttribute) {
+        int count = 0;
+        for (HtmlLabel label : labels) {
+            if (forAttribute.equals(label.getForAttribute())) {
+                count++;
+            }
+        }
+        return count == 1;
+    }
+
+}
---
+++ src/test/java/de/codescape/bitvunit/rule/forms/UniqueLabelForFormElementRuleTest.java
@@ -0,0 +1,67 @@
+package de.codescape.bitvunit.rule.forms;
+
+import de.codescape.bitvunit.rule.Violations;
+import org.junit.Test;
+
+import static de.codescape.bitvunit.test.Assertions.assertNoViolations;
+import static de.codescape.bitvunit.test.Assertions.assertViolations;
+import static de.codescape.bitvunit.test.HtmlPageCreator.createHtmlPage;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class UniqueLabelForFormElementRuleTest {
+
+    private final UniqueLabelForFormElementRule rule = new UniqueLabelForFormElementRule();
+
+    @Test
+    public void ruleHasAName() throws Exception {
+        assertNotNull(rule.getName());
+        assertEquals("UniqueLabelForFormElement", rule.getName());
+    }
+
+    @Test
+    public void singleLabelForFormElement() throws Exception {
+        String content = "<html>" +
+                "<body>" +
+                "  <form>" +
+                "    <label for=\"username\">Username</label>" +
+                "    <input type=\"text\" name=\"username\" id=\"username\" />" +
+                "  </form>" +
+                "</body>" +
+                "</html>";
+        Violations violations = rule.applyTo(createHtmlPage(content));
+        assertNoViolations(violations, rule);
+    }
+
+    @Test
+    public void twoLabelsForTwoDifferentFormElements() throws Exception {
+        String content = "<html>" +
+                "<body>" +
+                "  <form>" +
+                "    <label for=\"username\">Username</label>" +
+                "    <input type=\"text\" name=\"username\" id=\"username\" />" +
+                "    <label for=\"password\">Password</label>" +
+                "    <input type=\"password\" name=\"password\" id=\"password\" />" +
+                "  </form>" +
+                "</body>" +
+                "</html>";
+        Violations violations = rule.applyTo(createHtmlPage(content));
+        assertNoViolations(violations, rule);
+    }
+
+    @Test
+    public void duplicateLabelForFormElement() throws Exception {
+        String content = "<html>" +
+                "<body>" +
+                "  <form>" +
+                "    <label for=\"username\">Username</label>" +
+                "    <input type=\"text\" name=\"username\" id=\"username\" />" +
+                "    <label for=\"username\">Your Username</label>" +
+                "  </form>" +
+                "</body>" +
+                "</html>";
+        Violations violations = rule.applyTo(createHtmlPage(content));
+        assertViolations(violations, rule, 2);
+    }
+
+}
