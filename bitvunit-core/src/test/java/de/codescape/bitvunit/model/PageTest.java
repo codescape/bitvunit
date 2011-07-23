260f45738c6c7020e3b3333c79566e4076485d31
Stefan Glase <stefan.glase@googlemail.com>
2011-07-23 14:40:01 +0200

    improved test for method findAllHeadingTags()

--- bitvunit-core/src/test/java/de/codescape/bitvunit/model/PageTest.java
+++ bitvunit-core/src/test/java/de/codescape/bitvunit/model/PageTest.java
@@ -6,24 +6,28 @@ import org.junit.Test;
 import java.util.List;
 
 import static de.codescape.bitvunit.util.HtmlPageUtil.htmlPageFromString;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 public class PageTest {
 
     @Test
-    public void findAllHeadingTagsFindHeadingsOfEveryLevel() throws Exception {
-        String content = "<html><body>" +
-                "<h1>First Heading</h1>" +
-                "<h2>Second Heading</h2>" +
-                "<h3>Third Heading</h3>" +
-                "<h4>Fourth Heading</h4>" +
-                "<h5>Fifth Heading</h5>" +
-                "<h6>Sixth Heading</h6>" +
-                "</body></html>";
+    public void findAllHeadingTagsReturnsHeadingsOfEveryLevel() throws Exception {
+        String content = "<html><body>" + "<h1>First Heading</h1>" + "<h2>Second Heading</h2>" +
+                "<h3>Third Heading</h3>" + "<h4>Fourth Heading</h4>" + "<h5>Fifth Heading</h5>" +
+                "<h6>Sixth Heading</h6>" + "</body></html>";
         List<HtmlElement> headings = new Page(htmlPageFromString(content)).findAllHeadingTags();
         assertNotNull(headings);
         assertEquals(6, headings.size());
+
+        for (int i = 1; i < 7; i++) {
+            boolean found = false;
+            for (HtmlElement heading : headings) {
+                if (heading.getTagName().equals("h" + i)) {
+                    found = true;
+                }
+            }
+            assertTrue(found);
+        }
     }
 
 }
