1ebfdebb1dea7f386a988959972e5f8092ce541b
Stefan Glase <stefan.glase@googlemail.com>
2011-07-28 10:57:44 +0200

    added JavaDoc to ReportingContext

--- bitvunit-core/src/main/java/de/codescape/bitvunit/report/ReportingContext.java
+++ bitvunit-core/src/main/java/de/codescape/bitvunit/report/ReportingContext.java
@@ -1,7 +1,8 @@
 package de.codescape.bitvunit.report;
 
 /**
- * Singleton class that holds the configured {@link ReportWriter} to write out reports for any check that is executed.
+ * Singleton class holding the configured {@link ReportWriter} to write out reports for any accessibility check that is
+ * executed by the framework.
  *
  * @author Stefan Glase
  */
@@ -20,10 +21,20 @@ public class ReportingContext {
         return instance;
     }
 
+    /**
+     * Returns the {@link ReportWriter} to be used.
+     *
+     * @return {@link ReportWriter} to be used
+     */
     public static ReportWriter getReportWriter() {
         return getInstance().reportWriter;
     }
 
+    /**
+     * Sets the {@link ReportWriter} to be used.
+     *
+     * @param reportWriter {@link ReportWriter} to be used
+     */
     public static void setReportWriter(ReportWriter reportWriter) {
         getInstance().reportWriter = reportWriter;
     }
