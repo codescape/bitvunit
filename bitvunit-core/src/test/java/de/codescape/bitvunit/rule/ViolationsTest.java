fd570e60e799fdf2d04e76e252054812c317f933
Stefan Glase <stefan.glase@googlemail.com>
2011-08-29 14:15:11 +0200

    added tests for Violations

--- bitvunit-core/src/test/java/de/codescape/bitvunit/rule/ViolationsTest.java
+++ bitvunit-core/src/test/java/de/codescape/bitvunit/rule/ViolationsTest.java
@@ -4,6 +4,10 @@ import org.junit.Test;
 
 import static de.codescape.bitvunit.test.Assertions.assertNoViolations;
 import static de.codescape.bitvunit.test.Assertions.assertViolations;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.startsWith;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
 
 public class ViolationsTest {
@@ -18,6 +22,21 @@ public class ViolationsTest {
         Violations violations = new Violations();
         violations.add(someViolation());
         assertViolations(violations);
+    }
+
+    @Test
+    public void toStringForNoViolationsGivesCorrectFeedback() throws Exception {
+        Violations violations = new Violations();
+        assertEquals("No violations found.", violations.toString());
+    }
+
+    @Test
+    public void toStringForOneViolationGivesCorrectFeedback() throws Exception {
+        Violations violations = new Violations();
+        Violation violation = someViolation();
+        violations.add(violation);
+        assertThat(violations.toString(), startsWith("Found violations:"));
+        assertThat(violations.toString(), containsString(violation.toString()));
     }
 
     private Violation someViolation() {
