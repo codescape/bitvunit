ffea6226c4d67834ed477f8646cba9a9d1c56b75
Stefan Glase <stefan.glase@googlemail.com>
2011-09-05 10:37:51 +0200

    testing HtmlElementUtil

---
+++ bitvunit-core/src/test/java/de/codescape/bitvunit/util/html/HtmlElementUtilTest.java
@@ -0,0 +1,52 @@
+package de.codescape.bitvunit.util.html;
+
+import com.gargoylesoftware.htmlunit.html.HtmlElement;
+import org.junit.Test;
+
+import static de.codescape.bitvunit.util.html.HtmlPageUtil.toHtmlPage;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class HtmlElementUtilTest {
+
+    @Test
+    public void hasAttributeReturnsTrueIfAnEmptyAttributeExists() throws Exception {
+        HtmlElement elementWithEmptyAttribute = createHtmlElement("<img src=\"\" alt=\"\"/>");
+        assertTrue(HtmlElementUtil.hasAttribute(elementWithEmptyAttribute, "alt"));
+    }
+
+    @Test
+    public void hasAttributeReturnsTrueIfAnNonEmptyAttributeExists() throws Exception {
+        HtmlElement elementWithNonEmptyAttribute = createHtmlElement("<img src=\"\" alt=\"Hello World\"/>");
+        assertTrue(HtmlElementUtil.hasAttribute(elementWithNonEmptyAttribute, "alt"));
+    }
+
+    @Test
+    public void hasAttributeReturnsFalseIfTheAttributeDoesNotExist() throws Exception {
+        HtmlElement elementWithMissingAttribute = createHtmlElement("<img src=\"\"/>");
+        assertFalse(HtmlElementUtil.hasAttribute(elementWithMissingAttribute, "alt"));
+    }
+
+    @Test
+    public void hasNonEmptyAttributeReturnsFalseIfAnEmptyAttributeExists() throws Exception {
+        HtmlElement elementWithEmptyAttribute = createHtmlElement("<img src=\"\" alt=\"\"/>");
+        assertFalse(HtmlElementUtil.hasNonEmptyAttribute(elementWithEmptyAttribute, "alt"));
+    }
+
+    @Test
+    public void hasNonEmptyAttributeReturnsTrueIfAnNonEmptyAttributeExists() throws Exception {
+        HtmlElement elementWithNonEmptyAttribute = createHtmlElement("<img src=\"\" alt=\"Hello World\"/>");
+        assertTrue(HtmlElementUtil.hasNonEmptyAttribute(elementWithNonEmptyAttribute, "alt"));
+    }
+
+    @Test
+    public void hasNonEmptyAttributeReturnsFalseIfTheAttributeDoesNotExist() throws Exception {
+        HtmlElement elementWithMissingAttribute = createHtmlElement("<img src=\"\"/>");
+        assertFalse(HtmlElementUtil.hasNonEmptyAttribute(elementWithMissingAttribute, "alt"));
+    }
+
+    private HtmlElement createHtmlElement(String element) {
+        return toHtmlPage("<html><body>" + element + "</body></html>").getBody().getChildElements().iterator().next();
+    }
+
+}
