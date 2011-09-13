dbe3e3abd3254d34b003e96e9c6d5e3739a4c831
Stefan Glase <stefan.glase@googlemail.com>
2011-09-13 21:26:52 +0200

    spelling

--- bitvunit-core/src/main/java/de/codescape/bitvunit/rule/interactions/AvoidMetaRefreshRule.java
+++ bitvunit-core/src/main/java/de/codescape/bitvunit/rule/interactions/AvoidMetaRefreshRule.java
@@ -7,9 +7,10 @@ import de.codescape.bitvunit.rule.Violations;
 
 /**
  * AvoidMetaRefreshRule ensures that there is no fixed refresh caused by the <code>&lt;meta http-equiv="refresh"
- * content="seconds; URL=target" /&gt;</code> tag within the given HTML document. Automatically updating pages may cause
- * significant accessibility problems to people that are disabled or that use technology that hinders normal
- * interactions patterns.
+ * content="seconds; URL=target" /&gt;</code> tag within the given HTML document.
+ * <p/>
+ * Automatically updating pages may cause significant accessibility problems to people who are disabled or who use
+ * technology that hinders normal interaction patterns.
  *
  * @author Stefan Glase
  * @since 0.2
