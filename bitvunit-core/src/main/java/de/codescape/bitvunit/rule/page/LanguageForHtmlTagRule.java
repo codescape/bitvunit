65842456fec9748d8f6b3d0bf3439fb26bc776be
Stefan Glase <stefan.glase@googlemail.com>
2011-09-18 23:13:38 +0200

    typography

--- bitvunit-core/src/main/java/de/codescape/bitvunit/rule/page/LanguageForHtmlTagRule.java
+++ bitvunit-core/src/main/java/de/codescape/bitvunit/rule/page/LanguageForHtmlTagRule.java
@@ -8,7 +8,7 @@ import de.codescape.bitvunit.rule.Violations;
 import static de.codescape.bitvunit.util.html.HtmlElementUtil.hasNonEmptyAttribute;
 
 /**
- * LanguageForHtmlTagRule ensures that the given HTML document provides its language information through the
+ * LanguageForHtmlTagRule ensures that the given HTML document provides it's language information through the
  * <code>lang</code> attribute on the <code>&lt;html/&gt;</code> tag.
  * <p/>
  * A valid example of a document in German language would look like this:
@@ -20,7 +20,7 @@ import static de.codescape.bitvunit.util.html.HtmlElementUtil.hasNonEmptyAttribu
 public class LanguageForHtmlTagRule extends AbstractRule {
 
     private static final String RULE_NAME = "LanguageForHtmlTag";
-    private static final String RULE_MESSAGE = "Every <html /> tag should communicate the main language of that page with its lang attribute.";
+    private static final String RULE_MESSAGE = "Every <html /> tag should communicate the main language of that page with it's lang attribute.";
     private static final String LANG_ATTRIBUTE = "lang";
 
     @Override
