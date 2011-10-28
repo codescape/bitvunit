7dea0fe85bc5a851ceadbdd97cda12d86c3a575b
Stefan Glase <stefan.glase@googlemail.com>
2011-10-28 09:59:07 +0200

    javadoc

--- bitvunit-core/src/main/java/de/codescape/bitvunit/rule/forms/package-info.java
+++ bitvunit-core/src/main/java/de/codescape/bitvunit/rule/forms/package-info.java
@@ -1,4 +1,4 @@
 /**
- * Provides {@link de.codescape.bitvunit.rule.Rule} implementations focusing on accessible forms and their accessibility.
+ * Provides {@link de.codescape.bitvunit.rule.Rule} implementations focusing on accessible forms.
  */
 package de.codescape.bitvunit.rule.forms;
