9fb1dea2d07f6d2821e2c9597ba1a004b83e144c
Stefan Glase <stefan.glase@googlemail.com>
2012-01-16 08:56:03 +0100

    renaming

--- bitvunit-core/src/test/java/de/codescape/bitvunit/rule/PriorityTest.java
+++ bitvunit-core/src/test/java/de/codescape/bitvunit/rule/PriorityTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class PriorityTests {
+public class PriorityTest {
 
     @Test
     public void canBeCreatedFromUppercaseStringNORMAL() throws Exception {
