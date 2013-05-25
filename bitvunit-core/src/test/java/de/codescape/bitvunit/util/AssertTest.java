f9751d0eb6a9e5856c8280c39ab94e1e2545a9a6
Stefan Glase <stefan.glase@googlemail.com>
2013-05-25 12:24:17 +0200

    add tests as guards for failure in previous build

--- bitvunit-core/src/test/java/de/codescape/bitvunit/util/AssertTest.java
+++ bitvunit-core/src/test/java/de/codescape/bitvunit/util/AssertTest.java
@@ -2,37 +2,60 @@ package de.codescape.bitvunit.util;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class AssertTest {
 
     @Test(expected = IllegalArgumentException.class)
-    public void notNullForNullThrowsIllegalArgumentException() throws Exception {
-        Assert.notNull(null);
+    public void notNullForNullThrowsIllegalArgumentException() {
+        Assert.notNull((Object)null);
     }
 
     @Test
-    public void notNullForNonNullThrowsNoException() throws Exception {
+    public void notNullForMessageAndNullThrowsIllegalArgumentException() {
+        try {
+            Assert.notNull("Message", null);
+        } catch (IllegalArgumentException e) {
+            assertEquals("Message", e.getMessage());
+        }
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void notNullForMultipleObjectsWithNullObjectThrowsIllegalArgumentException() {
+        Assert.notNull("", null, 1, null);
+    }
+
+    @Test
+    public void notNullForMultipleNonNullObjectsThrowsNoException() {
+        Assert.notNull(1, 1, 1, 1, 1);
+        Assert.notNull("", "2", "saSASD");
+        Assert.notNull(new Object(), new Object(), new Object());
+    }
+
+    @Test
+    public void notNullForNonNullThrowsNoException() {
         Assert.notNull(new Object());
         Assert.notNull("Hello World");
         Assert.notNull(1);
     }
 
     @Test
-    public void notEmptyForNonEmptyStringThrowsNoException() throws Exception {
+    public void notEmptyForNonEmptyStringThrowsNoException() {
         Assert.notEmpty("Hello World");
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void notEmptyForNullThrowsIllegalArgumentException() throws Exception {
+    public void notEmptyForNullThrowsIllegalArgumentException() {
         Assert.notEmpty(null);
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void notEmptyForEmptyStringThrowsIllegalArgumentException() throws Exception {
+    public void notEmptyForEmptyStringThrowsIllegalArgumentException() {
         Assert.notEmpty("");
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void notEmptyForWhitespacesOnlyThrowsIllegalArgumentException() throws Exception {
+    public void notEmptyForWhitespacesOnlyThrowsIllegalArgumentException() {
         Assert.notEmpty("    ");
     }
 
