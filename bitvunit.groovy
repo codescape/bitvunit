d64a2f9ebaa0cc9e31b7462c17d8f907f63ae149
Stefan Glase <stefan.glase@googlemail.com>
2013-09-18 20:31:57 +0200

    remove documentation generation from command line tool

--- bitvunit.groovy
+++ bitvunit.groovy
@@ -6,10 +6,11 @@
  * @author Stefan Glase
  * @since 0.6
  */
+
+/* command line parsing */
+
 def availableCommands = [
         'create-rule': 'Wizard that helps you to create new rules.',
-        'update-ruledocs': 'Target that generates rule documentation from javadoc.',
-        'update-apidocs': 'Generates the html based javadoc for bitvunit-core.',
 ]
 
 if (args.size() != 1 || !(args[0] in availableCommands.keySet())) {
@@ -43,6 +44,8 @@ if (args[0] == 'create-rule') {
 
     println 'Done. Happy Coding!'
 }
+
+/* helper methods */
 
 def addToRuleSet(rule) {
     def ruleSetFile = new File('./bitvunit-core/src/main/resources/rulesets/all-rules.xml')
@@ -85,124 +88,4 @@ def getAvailableCategories() {
 
 def getNextBitvUnitVersion() {
     new XmlSlurper().parse(new File('./pom.xml')).version.text().substring(0, 3)
-}
-
-/* update-ruledocs */
-
-if (args[0] == 'update-ruledocs') {
-    print 'Path to gh-pages: '
-    def path = getUserInput({ new File(it).exists() && it.endsWith('gh-pages') })
-
-    def docsFile = createDocs(path)
-    println "Generated docs at ${docsFile}"
-}
-
-def createDocs(path) {
-    def target = "${path}/rules"
-
-    // create rules directory
-    def docsDir = new File("${target}/")
-    docsDir.deleteDir()
-    docsDir.mkdir()
-
-    // create rules index file
-    def rulesIndex = fileFromTemplate("./bitvunit-templates/docs/RulesIndex.template", [:], "${target}/index.textile")
-
-    new File('./bitvunit-core/src/main/java/de/codescape/bitvunit/rule/').eachDir { dir ->
-        println "Processing category ${dir.name}"
-
-        // create category index file
-        def categoryIndex = fileFromTemplate("./bitvunit-templates/docs/CategoryIndex.template", [name: dir.name], "${target}/${dir.name}.textile")
-
-        def rulesInCategory = 0
-        dir.eachFileMatch(~/^[A-Z]([A-Za-z])+Rule.java$/) { file ->
-            println "Processing file ${file.name}"
-            def rule = extractRuleData(file)
-            categoryIndex.text = categoryIndex.text + "\n" + fromTemplate("./bitvunit-templates/docs/RuleDoc.template", rule)
-            rulesInCategory++
-        }
-
-        rulesIndex.text += """\n* "${dir.name.capitalize()}":/rules/${dir.name} (${rulesInCategory} ${rulesInCategory > 1 ? 'rules' : 'rule'})"""
-    }
-
-    target
-}
-
-def fromTemplate(template, data = [:]) {
-    new groovy.text.SimpleTemplateEngine().createTemplate(new File(template).text).make(data).toString()
-}
-
-def fileFromTemplate(template, data, filename) {
-    def file = new File(filename)
-    file.createNewFile()
-    file.text = fromTemplate(template, data)
-    file
-}
-
-def extractRuleData(file) {
-    def name = extractRuleName(file)
-    def author = extractRuleAuthor(file)
-    def version = extractRuleVersion(file)
-    def description = extractRuleDescription(file)
-    [name: name, author: author, version: version, description: description]
-}
-
-def extractRuleName(file) {
-    (file.name - '.java').trim()
-}
-
-def extractRuleDescription(file) {
-    def description = ''
-    file.eachLine { line ->
-        if (line.startsWith(' *') && !(line.startsWith(' * @') || line.startsWith(' */'))) {
-            description += line.replaceAll('&lt;', '<').replaceAll('&gt;', '>') - ' *'
-        }
-    }
-    description?.trim() ?: 'missing'
-}
-
-def extractRuleVersion(file) {
-    def version
-    file.eachLine { line ->
-        if (line.startsWith(' * @since')) {
-            version = line - ' * @since'
-        }
-    }
-    version?.trim() ?: 'unknown'
-}
-
-def extractRuleAuthor(file) {
-    def author
-    file.eachLine { line ->
-        if (line.startsWith(' * @author')) {
-            author = line - ' * @author'
-        }
-    }
-    author?.trim() ?: 'unknown'
-}
-
-/* update-apidocs */
-
-if (args[0] == 'update-apidocs') {
-    print 'Path to gh-pages: '
-    def path = getUserInput({ new File(it).exists() && it.endsWith('gh-pages') })
-
-    def source = './bitvunit-core/target/apidocs'
-    def target = "${path}/apidocs"
-
-    println 'Generating apidocs...'
-    def goal = 'mvn -f ./bitvunit-core/pom.xml clean package javadoc:javadoc'.execute()
-    println goal.text
-
-    def ant = new AntBuilder()
-
-    println "Deleting old apidocs at ${target}..."
-    ant.delete(dir: "${target}", verbose: false)
-
-    println "Moving apidocs to ${target}..."
-    ant.copy(todir: target) {
-        fileset(dir: source)
-    }
-
-    println 'Done.'
 }
