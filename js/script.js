10e3494de87f80ff6c0c92cf4bae91a472c8a20c
Stefan Glase <stefan.glase@googlemail.com>
2013-10-06 15:59:35 +0200

    remove obsolete js

--- js/script.js
+++ js/script.js
@@ -1,21 +1,5 @@
 $(document).ready(function(){
-  // specific changelog requested?
-  var fragment = $.url().attr('fragment');
-    
-  // wrap each list in a div
-  $('h3.release + ul').wrap('<div class="changelog" />');
 
-  // hide each div and show the first or the requested
-  $('.changelog').hide();
-  if (!fragment) {
-    $('.changelog').first().show();
-  } else {
-    $("#" + fragment).next('.changelog').show();
-  }
-    
-  // add functionality to toggle the visibility
-  $('h3.release')
-    .css('cursor', 'pointer')
-    .click(function() { $(this).next('.changelog').slideToggle(); })
-    .attr('title', function() { return 'Toggle visibility of details for ' + $(this).text(); });
+  // do some stuff...
+
 });
