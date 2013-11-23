db5e40a4ab0eaf3628bf0a7438862ddb017f736c
Stefan Glase <stefan.glase@googlemail.com>
2013-11-23 23:38:30 +0100

    remove unused import

--- bitvunit-core/src/main/java/de/codescape/bitvunit/rule/text/AvoidBasefontTagRule.java
+++ bitvunit-core/src/main/java/de/codescape/bitvunit/rule/text/AvoidBasefontTagRule.java
@@ -5,8 +5,6 @@ import de.codescape.bitvunit.model.Page;
 import de.codescape.bitvunit.rule.AbstractRule;
 import de.codescape.bitvunit.rule.Violations;
 
-import java.util.List;
-
 /**
  * AvoidBasefontTagRule ensures that every <code>&lt;basefont/&gt;</code> element in the given HTML document leads to a
  * violation since this tag is marked as deprecated and should not be used anymore.
@@ -23,7 +21,7 @@ public class AvoidBasefontTagRule extends AbstractRule {
     public String getName() {
         return RULE_NAME;
     }
-    
+
     @Override
     protected void applyTo(Page page, Violations violations) {
         for (HtmlBaseFont baseFont : page.findAllBasefontTags()) {
