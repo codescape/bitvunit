ada070ec2be6f63a87beef39bff3f483ffc5e3ea
Stefan Glase <stefan.glase@googlemail.com>
2014-01-26 14:47:06 +0100

    add known issue and refactor code

--- bitvunit-core/src/main/java/de/codescape/bitvunit/rule/page/DoctypeForPageRule.java
+++ bitvunit-core/src/main/java/de/codescape/bitvunit/rule/page/DoctypeForPageRule.java
@@ -3,11 +3,13 @@ package de.codescape.bitvunit.rule.page;
 import de.codescape.bitvunit.model.Page;
 import de.codescape.bitvunit.rule.AbstractRule;
 import de.codescape.bitvunit.rule.Violations;
-import org.w3c.dom.DocumentType;
 
 /**
  * DoctypeForPageRule ensures that every page includes a <code>&lt;!DOCTYPE /&gt;</code> element to support rendering
  * and validation of this page by browsers and other viewers or interpreters.
+ * <p/>
+ * Known issue: Instances of WebDriver do not know about the DOCTYPE of the original document. So this rule cannot be
+ * applied successfully.
  *
  * @author Stefan Glase
  * @since 0.10
@@ -24,8 +26,7 @@ public class DoctypeForPageRule extends AbstractRule {
 
     @Override
     protected void applyTo(Page page, Violations violations) {
-        DocumentType doctype = page.getDoctype();
-        if (doctype == null) {
+        if (page.getDoctype() == null) {
             violations.add(createViolation(null, page, RULE_MESSAGE));
         }
     }
