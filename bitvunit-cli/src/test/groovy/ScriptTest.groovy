e59fedc33ac63849797e44bd4d1688c406f82296
Stefan Glase <stefan.glase@googlemail.com>
2014-06-08 20:16:51 +0200

    correct format of spock test

--- bitvunit-cli/src/test/groovy/ScriptTest.groovy
+++ bitvunit-cli/src/test/groovy/ScriptTest.groovy
@@ -1,10 +1,7 @@
-import org.junit.Before
-import org.junit.Ignore
-import org.junit.Test
+import spock.lang.Ignore
 import spock.lang.Shared
 import spock.lang.Specification
 
-@Ignore("test runs from the IDE but not during the Maven build because of a wrong path to the script")
 class ScriptTest extends Specification {
 
     static final String SCRIPT = 'bitvunit-cli/src/main/groovy/bitvunit.groovy'
@@ -12,21 +9,20 @@ class ScriptTest extends Specification {
     @Shared
     def script
 
-    @Before
     void setup() {
         script = new GroovyShell(new Binding()).parse(new File(SCRIPT))
     }
 
-    @Test
+    @Ignore("until classpath issues when calling from console are resolved")
     void "should determine major and minor version information for next version number"() {
         expect:
         script.invokeMethod('getNextBitvUnitVersion', null) ==~ /\d+\.\d+/
     }
 
-    @Test
+    @Ignore("until classpath issues when calling from console are resolved")
     void "should find rule categories to help choosing a category for newly created rules"() {
         expect:
-        script.invokeMethod('getAvailableCategories', null).containsAll(['forms','links', 'media'])
+        script.invokeMethod('getAvailableCategories', null).containsAll(['forms', 'links', 'media'])
     }
 
 }
