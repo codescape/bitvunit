7c8570991c60f7cfbb15409022f44baa570bda7e
Stefan Glase <stefan.glase@googlemail.com>
2014-08-03 15:12:41 +0200

    comply against Testable and not Rule and RuleSet anymore

--- bitvunit-core/src/main/java/de/codescape/bitvunit/hamcrest/ComplianceMatcher.java
+++ bitvunit-core/src/main/java/de/codescape/bitvunit/hamcrest/ComplianceMatcher.java
@@ -1,9 +1,7 @@
 package de.codescape.bitvunit.hamcrest;
 
-import de.codescape.bitvunit.rule.Rule;
+import de.codescape.bitvunit.Testable;
 import de.codescape.bitvunit.rule.Violations;
-import de.codescape.bitvunit.ruleset.BasicRuleSet;
-import de.codescape.bitvunit.ruleset.RuleSet;
 import org.hamcrest.Description;
 import org.hamcrest.Factory;
 import org.hamcrest.Matcher;
@@ -13,14 +11,14 @@ import static de.codescape.bitvunit.util.Assert.notNull;
 import static de.codescape.bitvunit.util.html.HtmlPageUtil.toHtmlPage;
 
 /**
- * Hamcrest matcher to be used to run accessibility checks against a single {@link Rule} or a {@link RuleSet}.
- * <p>
+ * Hamcrest matcher to be used to run accessibility checks against the {@link de.codescape.bitvunit.Testable} rule(s).
+ * <p/>
  * <b>Usage examples:</b>
  * <pre><code>
  * assertThat(supportedType, is(compliantTo(ruleSet)));
  * assertThat(supportedType, is(compliantTo(rule)));
  * </code></pre>
- * <p>
+ * <p/>
  * Have a look at {@link de.codescape.bitvunit.util.html.HtmlPageUtil} methods for supported types.
  *
  * @param <T> one of the supported types
@@ -30,27 +28,27 @@ import static de.codescape.bitvunit.util.html.HtmlPageUtil.toHtmlPage;
  */
 public class ComplianceMatcher<T> extends TypeSafeMatcher<T> {
 
-    private final RuleSet ruleSet;
+    private final Testable testable;
     private Violations violations;
 
     /**
-     * Creates a new {@link ComplianceMatcher} against the provided {@link RuleSet}.
+     * Creates a new {@link ComplianceMatcher} against the provided {@link de.codescape.bitvunit.Testable}.
      *
-     * @param ruleSet the {@link RuleSet} to be used
+     * @param testable the {@link de.codescape.bitvunit.Testable} rule(s) to be used
      */
-    public ComplianceMatcher(RuleSet ruleSet) {
-        this.ruleSet = ruleSet;
+    public ComplianceMatcher(Testable testable) {
+        this.testable = testable;
     }
 
     @Override
     protected boolean matchesSafely(T item) {
-        violations = ruleSet.applyTo(toHtmlPage(item));
+        violations = testable.applyTo(toHtmlPage(item));
         return !violations.hasViolations();
     }
 
     @Override
     public void describeTo(Description description) {
-        description.appendText("compliant to ").appendText(ruleSet.toString());
+        description.appendText("compliant to ").appendText(testable.toString());
     }
 
     @Override
@@ -60,29 +58,16 @@ public class ComplianceMatcher<T> extends TypeSafeMatcher<T> {
     }
 
     /**
-     * Returns a {@link ComplianceMatcher} that checks one of the supported types (see JavaDoc at class level) against
-     * the given {@link RuleSet}.
+     * Returns a {@link ComplianceMatcher} that checks one of the supported types against the
+     * {@link de.codescape.bitvunit.Testable} rule(s).
      *
-     * @param ruleSet {@link RuleSet} that should be used
-     * @param <T>     supported types are contained in JavaDoc at class level
-     * @return {@link ComplianceMatcher} to check against the {@link RuleSet}
+     * @param testable {@link de.codescape.bitvunit.Testable} that should be used
+     * @param <T>      supported types are contained in JavaDoc at class level
+     * @return {@link ComplianceMatcher} to check against the {@link de.codescape.bitvunit.Testable} rule(s)
      */
     @Factory
-    public static <T> Matcher<T> compliantTo(RuleSet ruleSet) {
-        return new ComplianceMatcher<T>(ruleSet);
-    }
-
-    /**
-     * Returns a {@link ComplianceMatcher} that checks one of the supported types (see JavaDoc at class level) against
-     * the given {@link Rule}.
-     *
-     * @param rule {@link Rule} that should be used
-     * @param <T>  supported types are contained in JavaDoc at class level
-     * @return {@link ComplianceMatcher} to check against the {@link Rule}
-     */
-    @Factory
-    public static <T> Matcher<T> compliantTo(Rule rule) {
-        return compliantTo(new BasicRuleSet(rule));
+    public static <T> Matcher<T> compliantTo(Testable testable) {
+        return new ComplianceMatcher<>(testable);
     }
 
 }
